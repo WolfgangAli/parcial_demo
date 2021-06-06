34bc28efaf43c233f2a918c4e16ff949c6e8a5a0
Wolfgang Alí <wolfgang520@outlook.com>
2021-06-05 18:34:56 -0600

    Add www directory and index.php file
    Add the www directory and index.php file. The index.php file only has the HTML part, it´s missing MySQL and Redis integration.

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Parcial Servidores</title>
+  </head> 
+  <body>
+
+    <h1 align = 'center'>Parcial Servidores: Listado de Estudiantes</h1>
+
+    <table align = 'center' border = '2'>        
+
+    </table>
+  </body>
+</html>
